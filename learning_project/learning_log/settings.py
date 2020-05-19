84199e16dbe32b0907bb3a477dc6e22b5d81ab68
wenbin.zheng <hg.wb.zheng@gmail.com>
2020-05-19 17:34:10 +0800

    feat: 1. add django demo with bootstrap 2. add requirements.txt

--- learning_project/learning_log/settings.py
+++ learning_project/learning_log/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 第三方应用程序
+    'bootstrap3',
     # 我的应用程序
     'learning_logs',
     'users',
@@ -124,3 +126,8 @@ STATIC_URL = '/static/'
 
 # TODO 我的设置 这个URL有问题，为什么要拼%5E，待解决
 LOGIN_URL = '/%5Eusers/login/'
+
+# django-bootstrap3的设置
+BOOTSTRAP3 = {
+    'include_jquery': True
+}
